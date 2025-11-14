282e110c5b342fcb9279830325cdbd780c551794
Blackeye <>
2025-11-14 16:55:11 +0100

    updated env

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-import dotenv from 'dotenv';
-dotenv.config();
 
-const base = process.env.VITE_BASE_URL
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }: { mode: string }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const base = process.env.VITE_BASE_URL
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  base: base
-})
+    base: base
+  })
+}
+
+
+// @ts-ignore
